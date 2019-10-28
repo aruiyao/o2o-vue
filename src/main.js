6c0b3cf7aa98af45ce49d3e13865ec9da5984d53
aruiyao <1023934747@qq.com>
2019-10-28 23:43:49 +0800

    添加elementUI组件

--- src/main.js
+++ src/main.js
@@ -2,10 +2,10 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import './plugins/element.js'
 
 Vue.config.productionTip = false;
-Vue.config.devtools = true;
-
+Vue.config.devtools = true;// 让chrome的F12中显示vue调试标签
 new Vue({
   router,
   store,
--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,13 @@
 module.exports = {
-  publicPath:"/dist",//设置项目根路径
+  productionSourceMap: true,
+  publicPath: "/dist", //设置项目根路径
   devServer: {
-    open: process.platform === 'darwin',
+    open: false, //是否自动打开浏览器
     host: '127.0.0.1',
     port: 9099,
     https: false,
     hotOnly: false,
-    proxy: {//设置代理
+    proxy: { //设置代理
       '/o2o': {
         target: 'http://127.0.0.1:8080/o2o',
         changeOrigin: true,
